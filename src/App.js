67849ac69c9e5f0a90c6ce18fc2fbeb6a3279236
Lich <TitaN-ToshA@yandex.com>
2022-06-24 12:47:20 +0300

    feat: добавил таймер

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from 'react';
 
 const hello = <h1>Дратуте!</h1>;
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Сейчас {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
@@ -11,6 +41,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           {hello}
+          <Clock />
         </p>
         <a
           className="App-link"
